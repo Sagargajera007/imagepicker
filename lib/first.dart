0cc6794b79a3df700e2def8aa89bca0e932487a4
Sagar Gajera <sagar194@gmail.com>
2022-08-05 08:08:56 +0530

    aa

--- lib/first.dart
+++ lib/first.dart
@@ -15,6 +15,7 @@ class _FirstState extends State<First> {
   final ImagePicker _picker = ImagePicker();
   String imagepath = "";
   List<XFile>? imageFileList = [];
+  bool Status = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +25,36 @@ class _FirstState extends State<First> {
       ),
       body: Column(
         children: [
-          Container(
-            height: 400,
-            width: 400,
-            child: imagepath.isEmpty
-                ? Image.asset('assets/images.png')
-                : Image.file(File(imagepath)),
-          ),
+          Status
+              ? Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 400,
+                      width: 400,
+                      child: imageFileList!.isEmpty
+                          ? Image.asset('assets/images.png')
+                          : GridView.builder(
+                              itemCount: imageFileList!.length,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3),
+                              itemBuilder: (BuildContext context, int index) {
+                                return Image.file(
+                                  File(imageFileList![index].path),
+                                  fit: BoxFit.cover,
+                                );
+                              }),
+                    ),
+                  ),
+                )
+              : Container(
+                  height: 400,
+                  width: 400,
+                  child: imagepath.isEmpty
+                      ? Image.asset('assets/images.png')
+                      : Image.file(File(imagepath)),
+                ),
           TextButton(
             onPressed: () {
               showDialog(
@@ -43,34 +67,44 @@ class _FirstState extends State<First> {
                         leading: Icon(Icons.camera_alt),
                         title: Text("Camera"),
                         onTap: () async {
-                          final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
-
+                          final XFile? photo = await _picker.pickImage(
+                              source: ImageSource.camera);
+                          Status = false;
                           if (photo != null) {
                             imagepath = photo.path;
                             setState(() {});
                           }
                         },
                       ),
-                      ListTile(leading: Icon(Icons.photo),
-                      title: Text("Gallery"),
-                      onTap: () async {
-                        final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
-                        if(photo != null){
-                          imagepath = photo.path;
-                          setState(() {
-                          });
-                        }
-                      },),
-                      ListTile(leading: Icon(Icons.photo),
+                      ListTile(
+                        leading: Icon(Icons.photo),
+                        title: Text("Gallery"),
+                        onTap: () async {
+                          final XFile? photo = await _picker.pickImage(
+                              source: ImageSource.gallery);
+                          Status = false;
+                          if (photo != null) {
+                            imagepath = photo.path;
+                            setState(() {});
+                          }
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.photo),
                         title: Text("Multiple Photos"),
                         onTap: () async {
-                          final List<XFile>? selectedImages = await _picker.pickMultiImage();
+                          final List<XFile>? selectedImages =
+                              await _picker.pickMultiImage();
+                          imageFileList = [];
                           if (selectedImages!.isNotEmpty) {
                             imageFileList!.addAll(selectedImages);
+                            Status = true;
                           }
-                          print("Image List Length:" + imageFileList!.length.toString());
-                          setState((){});
-                        },)
+                          print("Image List Length:" +
+                              imageFileList!.length.toString());
+                          setState(() {});
+                        },
+                      )
                     ],
                   );
                 },
